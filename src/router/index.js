a456fff3e29289bd2f20cee9b9b76be38b9293b8
lee <zhengdangnianshao@qq.com>
2017-04-24 20:03:45 +0800

    lizhikai 4/24

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ export default new Router({
               path: '/wode',
               name:'wode',
               component: resolve => require(['../components/page/wode.vue'], resolve)
-          }
+          },
 
             {
               path: '/lzk',
